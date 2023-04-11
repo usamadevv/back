d003ab9ab2d06817afd6ce273d543975462183af
MacBook <macbook@owners-MacBook-Pro.local>
2023-04-11 06:45:42 +0500

    first commit

---
+++ Models/Supervisor.js
@@ -0,0 +1,45 @@
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Sueprvisor = new Schema({
+   
+   
+    name: {
+        type: String,
+        
+    }
+    ,
+    siteid:{
+        type:String,
+    },
+    sitename:{
+        type:String
+    }
+    ,
+    phone: {
+        type: String
+    },
+    
+    address: {
+        type: String
+    },
+    status: {
+        type: String
+    },
+    
+    pass: {
+        type: String
+    },
+    
+});
+
+
+
+
+
+
+const Super = mongoose.model('Sueprvisor', Sueprvisor);
+module.exports =Super
+  
